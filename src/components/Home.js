7fb6c9f5889d3e4efd26095edab586cd47b2a73b
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-05-21 08:01:06 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { withUserContext } from "./UserContext";
 import { withStitch } from "./Stitch";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   componentDidMount() {
@@ -8,26 +9,57 @@ class Home extends Component {
   }
   render() {
     return (
-      <div>
-        <div>this is home page </div>
-        <button
-          onClick={() => {
-            this.props.user.updateContext("Hello");
-          }}
+      <div
+        className="row m-0 justify-content-center"
+        style={{ width: "100vw" }}
+      >
+        <div
+          className="d-flex col-12 py-5 px-0 justify-content-center"
+          style={{ height: "100vh" }}
         >
-          update user
-        </button>
-        <button
-          onClick={async () => {
-            let user = await this.props.stitch.logInAnonymously();
-
-            console.log(user);
-            this.props.user.updateContext(user);
-            console.log(this.props);
-          }}
+          <div
+            className="d-flex row btn shadow my-auto  justify-content-center px-0"
+            style={{
+              height: "50vh",
+              width: "100vw",
+              backgroundColor: "rgba(50,50,50,0.5)",
+            }}
+          >
+            <div
+              className="col-12 px-0 display-4 text-light"
+              style={{
+                fontSize: "6vw",
+              }}
+            >
+              EMPLOYEE MANAGEMENT SYSTEM
+            </div>
+            <div
+              className="col-12 display-5"
+              style={{ fontSize: "3vw", color: "darkgray" }}
+            >
+              Performance Reviews, Real-Time Feedback, Goals, SCRUM,
+              Communication
+            </div>
+            <Link
+              className="btn px-0 btn-dark text-light"
+              style={{ height: "7vw", width: "40vw" }}
+            >
+              <div style={{ fontSize: "4vw" }}>Click Here for Demo</div>
+            </Link>
+          </div>
+        </div>
+        <div
+          className="d-flex col-12 py-5 px-0 justify-content-center"
+          style={{ height: "100vh" }}
         >
-          login anonymously
-        </button>
+          <div
+            className="d-flex bg-light row btn shadow my-auto  justify-content-center px-0"
+            style={{
+              height: "100vh",
+              width: "100vw",
+            }}
+          ></div>
+        </div>
       </div>
     );
   }
