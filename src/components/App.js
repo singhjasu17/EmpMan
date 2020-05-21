1d07d5ce20a138c7fc3dab3efee0809a09e61a97
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-05-21 08:00:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { withUserContext } from "./UserContext";
 import { withStitch } from "./Stitch";
 
@@ -14,82 +14,89 @@ import ResetPassword from "./ResetPassword";
 import Navbar from "./Navbar";
 import FirstLoginData from "./FirstLoginData";
 
-import User, { UserContext } from "./UserContext";
-import StitchClass, { StitchContext } from "./Stitch";
-import Profile from './Profile'
-import InputFields from './InputFields' 
-let example=null;
+import bg from "./ConfRoom.jpg";
+
+import { UserContext } from "./UserContext";
+import Profile from "./Profile";
+import InputFields from "./InputFields";
+let example = null;
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      user:null,
-     
-    }
+    this.state = {
+      user: null,
+    };
   }
-   componentDidMount()
-   {
-     this.updateUserData()
+  componentDidMount() {
+    this.updateUserData();
   }
-  updateUserData=async ()=>
-  {
-
-    if(this.props.stitch.client.auth.currentUser)
-    {
-    console.log("Refreshing user data on page refresh")
-    let res=await this.props.stitch.client.auth.refreshCustomData()
-    console.log("current User"+this.props.stitch.client.auth.currentUser)
-    this.setState({user:this.props.stitch.client.auth.currentUser.customData})
+  updateUserData = async () => {
+    if (this.props.stitch.client.auth.currentUser) {
+      console.log("Refreshing user data on page refresh");
+      let res = await this.props.stitch.client.auth.refreshCustomData();
+      console.log("current User" + this.props.stitch.client.auth.currentUser);
+      this.setState({
+        user: this.props.stitch.client.auth.currentUser.customData,
+      });
+    } else {
+      console.log("user not logged in ");
     }
-    else
-    {
-      console.log("user not logged in ")
-    }
-  }
+  };
   updateValue = (val) => {
-    this.setState({user:val});
- }
-  
+    this.setState({ user: val });
+  };
+
   render() {
-
-    example=this.state.user;
-    console.log(this.state.user)
+    example = this.state.user;
+    console.log(this.state.user);
     return (
+      <UserContext.Provider
+        value={{ state: this.state, updateUser: this.updateValue }}
+      >
+        <Router basename="/">
+          <div style={{ minHeight: "100vh" }}>
+            <img
+              src={bg}
+              style={{
+                height: "100vh",
+                width: "100%",
+                zIndex: -1,
+                position: "fixed",
 
-    <UserContext.Provider value={{state:this.state,updateUser:this.updateValue}}>
-
-      <Router basename="/">
-        <div>
-          <Navbar />
-          <Switch>
-            <Route path="/SignUp">
-              <SignUp />
-            </Route>
-            <Route path="/SignIn">
-              <SignIn />
-            </Route>
-            <Route path="/FirstLoginData">
-              <FirstLoginData />
-            </Route>
-            <Route path="/InputFields">
-              <InputFields />
-            </Route>
-            <Route path="/Profile">
-              <Profile />
-            </Route>
-            <Route path="/ConfirmMail" component={ConfirmEmail} />
-            <Route path="/ResetPassword">
-              <ResetPassword />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
- 
-    </UserContext.Provider>
-
+                objectFit: "cover",
+              }}
+            ></img>
+            <Navbar />
+            <Switch>
+              <Route path="/SignUp">
+                <SignUp />
+              </Route>
+              <Route path="/SignIn">
+                <SignIn />
+              </Route>
+              <Route path="/FirstLoginData">
+                <FirstLoginData />
+              </Route>
+              <Route path="/InputFields">
+                <InputFields />
+              </Route>
+              <Route path="/Profile">
+                <Profile />
+              </Route>
+              <Route path="/ConfirmMail" component={ConfirmEmail} />
+              <Route path="/ResetPassword">
+                <ResetPassword />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </UserContext.Provider>
     );
   }
 }
-export {example}
+export { example };
 export default withStitch(withUserContext(App));
